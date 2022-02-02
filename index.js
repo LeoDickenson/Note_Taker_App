cdd65c4d7b39d45fb414164d113b15d77ce63ae7
Leo Dickens <dickens.leo@gmail.com>
2022-02-01 18:28:10 -0600

    server set up complete

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+// Helper method for generating unique ids
+// const uuid = require('./helpers/uuid');
+// const reviews = require('./db/reviews');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
